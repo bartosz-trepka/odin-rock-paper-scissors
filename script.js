c7eb2b31fe7bd4771023e3a5746f6d07c1eeb8ea
Bartosz Trepka <bartosz.trepka@gmail.com>
2022-05-02 23:48:24 +0200

    Update script.js - functions playRound() & message()

--- script.js
+++ script.js
@@ -2,12 +2,52 @@ function computerPlay() {
     let computerChoice = Math.floor(Math.random() * 3);
 
     if (computerChoice === 0) {
-        console.log("Rock");
+        return "rock";
     } else if (computerChoice === 1) {
-        console.log("Paper");
+        return "paper";
     } else {
-        console.log("Scissors");
+        return "scissors";
     }
 }
 
-computerPlay();
+function playRound(player, computer) {
+    if (player === computer) {
+        return message("draw");
+    }
+    if (player === "rock") {
+        if (computer === "scissors") {
+            return message("win");
+        } else {
+            return message("lose");
+        }
+    }
+    if (player === "paper") {
+        if (computer === "rock") {
+            return message("win");
+        } else {
+            return message("lose");
+        }
+    }
+    if (player === "scissors") {
+        if (computer === "paper") {
+            return message("win");
+        } else {
+            return message("lose");
+        }
+    }
+}
+
+function message(result) {
+    if (result === "win") {
+        return `You win! (P: ${playerSelection} vs C: ${computerSelection})`;
+    } else if (result === "lose") {
+        return `You lose! (P: ${playerSelection} vs C: ${computerSelection})`;
+    } else {
+        return `It's a draw! (P: ${playerSelection} vs C: ${computerSelection})`;
+    }
+}
+
+const playerSelection = "Rock".toLowerCase();
+const computerSelection = computerPlay().toLowerCase();
+
+console.log(playRound(playerSelection, computerSelection));
