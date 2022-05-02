135713746660ccf144925750bddf3403000f8b08
Bartosz Trepka <bartosz.trepka@gmail.com>
2022-05-02 22:42:56 +0200

    Update script.js - function computerPlay()

---
+++ script.js
@@ -0,0 +1,13 @@
+function computerPlay() {
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    if (computerChoice === 0) {
+        console.log("Rock");
+    } else if (computerChoice === 1) {
+        console.log("Paper");
+    } else {
+        console.log("Scissors");
+    }
+}
+
+computerPlay();
